89663bef87bd1c9119857eccf60259cf13189fdb
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-05-30 07:16:03 +0200

    NavBar - remove Container and make font bold

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
@@ -8,13 +7,11 @@ const NavBar = () => {
   return (
     <Box sx={{ mb: 10 }}>
       <AppBar>
-        <Container maxWidth="lg">
-          <Toolbar>
-            <Typography variant="h5" component="h1" fontWeight="bold">
-              kisi
-            </Typography>
-          </Toolbar>
-        </Container>
+        <Toolbar>
+          <Typography variant="h5" component="h1" fontWeight="bold">
+            kisi
+          </Typography>
+        </Toolbar>
       </AppBar>
     </Box>
   );
