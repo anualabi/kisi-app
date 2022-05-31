14bba2d815195e4b8ece06dcfb2796f145857870
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2022-05-31 11:40:48 +0200

    NotFound -  Delete component from application

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Groups from './components/Groups';
 import GroupLocks from './components/GroupLocks';
-import NotFound from './components/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Groups />}></Route>
         <Route path="/groupLocks/:groupId" element={<GroupLocks />}></Route>
       </Routes>
